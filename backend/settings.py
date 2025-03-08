2898fa844a9bc93a66263bbd0c53f00bd853c68a
alireza <alireza.yousefpourm@gmail.com>
2025-03-08 23:37:29 +0330

    change sqlite to mysql

--- backend/settings.py
+++ backend/settings.py
@@ -107,8 +107,12 @@ MIDDLEWARE = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'test'),
+        'USER': os.getenv('DB_USER', 'asdf'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'asdf'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     },
 }
 
