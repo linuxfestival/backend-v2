cb02732a3939877239c739b2a5bf9afa54f2b8e1
alireza <alireza.yousefpourm@gmail.com>
2025-03-18 23:08:07 +0330

    fix(settings): Fix static files path

--- backend/settings.py
+++ backend/settings.py
@@ -36,9 +36,6 @@ LOGIN_URL = 'accounts:token'
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
-]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
