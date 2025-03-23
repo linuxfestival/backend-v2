c0e673d52ca1e8b50bf04065d016aaea2473f1bb
alireza <alireza.yousefpourm@gmail.com>
2025-03-23 23:11:12 +0330

    fix(admin): Fix invalid Admin panel model for user

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Staff
 
 
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(admin.ModelAdmin):
     ordering = ['date_joined']
     list_display = ['phone_number', 'first_name', 'last_name', 'email', 'is_staff', 'is_active']
     list_filter = ['is_staff', 'is_active', 'date_joined']
